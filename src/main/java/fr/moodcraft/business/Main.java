0fb975432687f22835329f68ecd06ae7dfae7f49
jeanmarieschintu89-max <jeanmarieschintu89@gmail.com>
2026-05-12 15:30:44 +0200

    Create src/main/java/fr/moodcraft/business/Main.java

---
+++ src/main/java/fr/moodcraft/business/Main.java
@@ -0,0 +1,93 @@
+package fr.moodcraft.business;
+
+import fr.moodcraft.business.command.BusinessCommand;
+import fr.moodcraft.business.command.ContractCommand;
+import fr.moodcraft.business.command.RequestsCommand;
+
+import fr.moodcraft.business.util.VaultHook;
+
+import org.bukkit.Bukkit;
+
+import org.bukkit.command.PluginCommand;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+    private static Main instance;
+
+    @Override
+    public void onEnable() {
+
+        instance = this;
+
+        saveDefaultConfig();
+
+        VaultHook.setup();
+
+        registerCommands();
+
+        Bukkit.getConsoleSender().sendMessage("");
+        Bukkit.getConsoleSender().sendMessage(
+                "§8----- §6✦ §aMood§6Business §6✦ §8-----"
+        );
+        Bukkit.getConsoleSender().sendMessage(
+                "§a✔ §fRegistre économique chargé."
+        );
+        Bukkit.getConsoleSender().sendMessage(
+                "§7Service officiel de §aMood§6Craft§7."
+        );
+        Bukkit.getConsoleSender().sendMessage("");
+    }
+
+    @Override
+    public void onDisable() {
+
+        Bukkit.getConsoleSender().sendMessage("");
+        Bukkit.getConsoleSender().sendMessage(
+                "§8----- §6✦ §aMood§6Business §6✦ §8-----"
+        );
+        Bukkit.getConsoleSender().sendMessage(
+                "§c✘ §fRegistre économique arrêté."
+        );
+        Bukkit.getConsoleSender().sendMessage("");
+    }
+
+    private void registerCommands() {
+
+        PluginCommand entreprise =
+                getCommand("entreprise");
+
+        if (entreprise != null) {
+
+            entreprise.setExecutor(
+                    new BusinessCommand()
+            );
+        }
+
+        PluginCommand demandes =
+                getCommand("demandes");
+
+        if (demandes != null) {
+
+            demandes.setExecutor(
+                    new RequestsCommand()
+            );
+        }
+
+        PluginCommand contrat =
+                getCommand("contrat");
+
+        if (contrat != null) {
+
+            contrat.setExecutor(
+                    new ContractCommand()
+            );
+        }
+    }
+
+    public static Main getInstance() {
+
+        return instance;
+    }
+}
