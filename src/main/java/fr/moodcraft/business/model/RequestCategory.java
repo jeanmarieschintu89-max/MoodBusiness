b18f638f0eba297624843677180ce12b6fe0cde1
jeanmarieschintu89-max <jeanmarieschintu89@gmail.com>
2026-05-17 04:12:31 +0200

    Simplify mission categories

--- src/main/java/fr/moodcraft/business/model/RequestCategory.java
+++ src/main/java/fr/moodcraft/business/model/RequestCategory.java
@@ -2,53 +2,18 @@ package fr.moodcraft.business.model;
 
 public enum RequestCategory {
 
-    CONSTRUCTION(
-            "§6Construction"
-    ),
-
-    LIVRAISON(
-            "§bLivraison"
-    ),
-
-    AGRICULTURE(
-            "§aAgriculture"
-    ),
-
-    MINAGE(
-            "§7Récolte / minage"
-    ),
-
-    COMMERCE(
-            "§eCommerce"
-    ),
-
-    IMMOBILIER(
-            "§dImmobilier"
-    ),
-
-    EVENEMENT(
-            "§5Événement"
-    ),
-
-    SERVICE(
-            "§fService"
-    ),
-
-    AUTRE(
-            "§8Autre"
-    );
+    CONSTRUCTION("§6Construction"),
+    RESSOURCES("§bRessources"),
+    SERVICE("§fService"),
+    AUTRE("§8Autre");
 
     private final String displayName;
 
-    RequestCategory(
-            String displayName
-    ) {
-
+    RequestCategory(String displayName) {
         this.displayName = displayName;
     }
 
     public String getDisplayName() {
-
         return displayName;
     }
 }
